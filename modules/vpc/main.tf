a53411c61dbc78bed172e4819221406fade67536
siva <sivaram0434@gmail.com>
2025-05-27 17:26:05 +0530

    add the conditions

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -143,16 +143,16 @@ resource "aws_route" "public_internet" {
 
 resource "aws_route" "private_internet" {
   route_table_id            = aws_route_table.private.id
-  nat_gateway_id  = aws_nat_gateway.example[count.index].id
+  nat_gateway_id  = aws_nat_gateway.example[0].id
   destination_cidr_block   = "0.0.0.0/0"
 }
 resource "aws_route" "private_nat" {
   route_table_id            = aws_route_table.private.id
-  nat_gateway_id  = aws_nat_gateway.example[count.index].id
+  nat_gateway_id  = aws_nat_gateway.example[0].id
   destination_cidr_block   = "0.0.0.0/0"
 }
 resource "aws_route" "db_nat" {
   route_table_id            = aws_route_table.db.id
-  nat_gateway_id  = aws_nat_gateway.example[count.index].id
+  nat_gateway_id  = aws_nat_gateway.example[0].id
   destination_cidr_block   = "0.0.0.0/0"
 }
