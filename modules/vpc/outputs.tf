2b9cd14bf4fe06e7627287799139e7612c3ad6fb
siva <sivaram0434@gmail.com>
2025-05-30 15:57:55 +0530

    rectify the sysntactical error

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -17,4 +17,5 @@ output "db_subnet_ids" {
 
 output "sg_id" {
     description = "The ID of the security group"
-    value       = aws_security_group.allow_all.id}
+    value       = aws_security_group.allow_all.id
+}
